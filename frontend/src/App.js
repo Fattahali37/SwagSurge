97559bd470665d141bba2e405f1980dc9a029258
hubaidurrehman24 <hubaidurrehman03@gmail.com>
2024-04-17 14:43:46 +0500

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
       
+    <Navbar/>
+    </BrowserRouter>
     </div>
   );
 }
