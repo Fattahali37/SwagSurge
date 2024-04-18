df5976e2740d6d6572a5c6919b10e1ca1dcd29e3
Fattahali37 <fattahali37@gmail.com>
2024-04-18 12:01:02 +0500

    popular men items

---
+++ frontend/src/Components/Assets/all_products.js
@@ -0,0 +1,40 @@
+import p1_product from "./p1.jpg";
+import p2_product from "./p2.jpg";
+import p3_product from "./p3.jpg";
+import p4_product from "./p4.jpg";
+
+let all_product = [
+    {
+        id: 1,
+        name: "Shirt1",
+        category: "men",
+        image: p1_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 2,
+        name: "Shirt2",
+        category: "men",
+        image: p2_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 3,
+        name: "Shirt3",
+        category: "men",
+        image: p3_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 4,
+        name: "Shirt4",
+        category: "men",
+        image: p4_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    }
+];
+
