51bc5834bc404a4a4689e3c13255990a57b39ed9
Fattahali37 <fattahali37@gmail.com>
2024-04-18 16:39:09 +0500

    images changed for woman popular section
    size need to be fixed

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -1,7 +1,7 @@
-import p1_product from "./p1.jpg";
-import p2_product from "./p2.jpg";
-import p3_product from "./p3.jpg";
-import p4_product from "./p4.jpg";
+import p1_product from "./woman1.png";
+import p2_product from "./woman2.png";
+import p3_product from "./woman3.png";
+import p4_product from "./woman4.png";
 
 let data_product = [
     {
