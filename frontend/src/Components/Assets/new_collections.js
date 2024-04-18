bd36a868e53e2d75df2e90578936e17f50aa10ae
Fattahali37 <fattahali37@gmail.com>
2024-04-18 12:46:24 +0500

    New Collection Menu

---
+++ frontend/src/Components/Assets/new_collections.js
@@ -0,0 +1,37 @@
+import p1_product from "./p1.jpg";
+import p2_product from "./p2.jpg";
+import p3_product from "./p3.jpg";
+import p4_product from "./p4.jpg";
+
+let data_product = [
+    {
+        id: 1,
+        name: "Shirt1",
+        image: p1_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 2,
+        name: "Shirt2",
+        image: p2_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 3,
+        name: "Shirt3",
+        image: p3_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 4,
+        name: "Shirt4",
+        image: p4_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    }
+];
+
+export default data_product;
