ce8f5ca21fca8bc4b09b3071fb524651d9bd1ea2
hubaidurrehman24 <hubaidurrehman03@gmail.com>
2024-04-18 22:01:54 +0500

    new collcetion

--- frontend/src/Components/Assets/new_collections.js
+++ frontend/src/Components/Assets/new_collections.js
@@ -1,7 +1,16 @@
-import p1_product from "./newW1.png";
-import p2_product from "./newW2.png";
-import p3_product from "./newW3.png";
-import p4_product from "./newW4.png";
+import p1_product from "./newM1.png";
+import p2_product from "./newW1.png";
+import p3_product from "./newM2.png";
+import p4_product from "./newW2.png";
+import p5_product from "./newM3.png";
+import p6_product from "./newW3.png";
+import p7_product from "./newM4.png";
+import p8_product from "./newW4.png";
+
+
+
+
+
 
 let data_product = [
     {
@@ -29,6 +38,34 @@ let data_product = [
         id: 4,
         name: "Shirt4",
         image: p4_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 5,
+        name: "Shirt5",
+        image: p5_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 6,
+        name: "Shirt6",
+        image: p6_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 7,
+        name: "Shirt7",
+        image: p7_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 8,
+        name: "Shirt8",
+        image: p8_product,
         new_price: 50.0,
         old_price: 80.0,
     }
