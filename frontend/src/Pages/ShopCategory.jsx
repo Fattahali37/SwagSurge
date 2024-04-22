e1628909509103a036eeff69e19f50e98f21ffb0
Fattahali37 <fattahali37@gmail.com>
2024-04-22 20:52:48 +0500

    product rating is not visible along images

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -3,6 +3,7 @@ import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
 import dropdown_icon from '../Components/Assets/dropdown-icon.png'
 import Items from '../Components/Items/Items'
+
 const ShopCategory = (props) => {
   const {all_product}=useContext(ShopContext);
   return (
