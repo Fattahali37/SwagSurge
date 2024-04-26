3379128798888bf659504d4b4111b6d357bf58b3
Fattahali37 <fattahali37@gmail.com>
2024-04-26 15:59:55 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const muklter = require("multer");
+const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
 
