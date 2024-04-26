7b06274afa5a3fb20c7ce27d334b5142fcbb7564
Fattahali37 <fattahali37@gmail.com>
2024-04-26 13:50:46 +0500

    images added

--- frontend/src/Components/Assets/all_product.js
+++ frontend/src/Components/Assets/all_product.js
@@ -2,7 +2,42 @@ import p1_product from "./newM1.png";
 import p2_product from "./newM2.png";
 import p3_product from "./newM3.png";
 import p4_product from "./newM4.png";
-
+import p5_product from "./men1.jpg";
+import p6_product from "./men2.jpg";
+import p7_product from "./men3.jpg";
+import p8_product from "./men4.jpg";
+import p9_product from "./men5.jpg";
+import p10_product from "./men6.jpg";
+import p11_product from "./men7.jpg";
+import p12_product from "./men8.jpg";
+import p13_product from "./men9.jpg";
+import p14_product from "./men10.jpg";
+import p15_product from "./men11.jpg";
+import p16_product from "./men12.jpg";
+import p17_product from "./w1.jpg";
+import p18_product from "./w2.jpg";
+import p19_product from "./w3.jpg";
+import p20_product from "./w4.jpg";
+import p21_product from "./w5.jpg";
+import p22_product from "./w6.jpg";
+import p23_product from "./w7.jpg";
+import p24_product from "./w8.jpg";
+import p25_product from "./w9.jpg";
+import p26_product from "./w10.jpg";
+import p27_product from "./w11.jpg";
+import p28_product from "./w12.jpg";
+import p29_product from "./c1.jpg";
+import p30_product from "./c2.jpg";
+import p31_product from "./c3.jpg";
+import p32_product from "./c4.jpg";
+import p33_product from "./c5.jpg";
+import p34_product from "./c6.jpg";
+import p35_product from "./c7.jpg";
+import p36_product from "./c8.jpg";
+import p37_product from "./c9.jpg";
+import p38_product from "./c10.jpg";
+import p39_product from "./c11.jpg";
+import p40_product from "./c12.jpg";
 let all_product = [
     {
         id: 1,
@@ -31,8 +66,296 @@ let all_product = [
     {
         id: 4,
         name: "Shirt4",
-        category: "women",
+        category: "men",
         image: p4_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id:5,
+        name: "Shirt5",
+        category: "men",
+        image: p5_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 6,
+        name: "Shirt6",
+        category: "men",
+        image: p6_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 7,
+        name: "Shirt7",
+        category: "men",
+        image: p7_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 8,
+        name: "Shirt8",
+        category: "men",
+        image: p8_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 9,
+        name: "Shirt9",
+        category: "men",
+        image: p9_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 10,
+        name: "Shirt10",
+        category: "men",
+        image: p10_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 11,
+        name: "Shirt11",
+        category: "men",
+        image: p11_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 12,
+        name: "Shirt12",
+        category: "men",
+        image: p12_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 13,
+        name: "Shirt13",
+        category: "men",
+        image: p13_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id:14,
+        name: "Shirt14",
+        category: "men",
+        image: p14_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 15,
+        name: "Shirt15",
+        category: "men",
+        image: p15_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 16,
+        name: "Shirt16",
+        category: "men",
+        image: p16_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 17,
+        name: "Shirt1",
+        category: "women",
+        image: p17_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 18,
+        name: "Shirt2",
+        category: "women",
+        image: p18_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 19,
+        name: "Shirt3",
+        category: "women",
+        image: p19_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 20,
+        name: "Shirt4",
+        category: "women",
+        image: p20_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 21,
+        name: "Shirt5",
+        category: "women",
+        image: p21_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 22,
+        name: "Shirt6",
+        category: "women",
+        image: p22_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 23,
+        name: "Shirt7",
+        category: "women",
+        image: p23_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 24,
+        name: "Shirt8",
+        category: "women",
+        image: p24_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 25,
+        name: "Shirt9",
+        category: "women",
+        image: p25_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 26,
+        name: "Shirt10",
+        category: "women",
+        image: p26_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 27,
+        name: "Shirt11",
+        category: "women",
+        image: p27_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 28,
+        name: "Shirt12",
+        category: "women",
+        image: p28_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 29,
+        name: "Shirt1",
+        category: "kids",
+        image: p29_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 30,
+        name: "Shirt2",
+        category: "kids",
+        image: p30_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 31,
+        name: "Shirt3",
+        category: "kids",
+        image: p31_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 32,
+        name: "Shirt4",
+        category: "kids",
+        image: p32_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 33,
+        name: "Shirt5",
+        category: "kids",
+        image: p33_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 34,
+        name: "Shirt6",
+        category: "kids",
+        image: p34_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 35,
+        name: "Shirt7",
+        category: "kids",
+        image: p35_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 36,
+        name: "Shirt8",
+        category: "kids",
+        image: p36_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 37,
+        name: "Shirt9",
+        category: "kids",
+        image: p37_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 38,
+        name: "Shirt10",
+        category: "kids",
+        image: p38_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 39,
+        name: "Shirt11",
+        category: "kids",
+        image: p39_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 40,
+        name: "Shirt12",
+        category: "kids",
+        image: p40_product,
         new_price: 50.0,
         old_price: 80.0,
     }
