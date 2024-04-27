c7c413715e79dd7360b67b0970057331ec063733
Fattahali37 <fattahali37@gmail.com>
2024-04-27 11:19:59 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,46 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { error } = require("console");
+
 
 app.use(express.json());
 app.use(cors());
 
+//Database connectio with mongodb
+
+mongoose.connect("mongodb+srv://fattahali37:!babayaga37@cluster0.psbe9ke.mongodb.net/")
+
+//API creation:
+app.get("/",(req,res)=>{
+    res.send("Express App running");
+})
+
+//Image storage engine
+const storage = multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+
+//creating endpoint for images
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`hhtp:localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+app.listen(port,(error)=>{
+    if(!error){
+        console.log("server running on port "+port);
+    }
+    else{
+        console.log("Error: "+error);
+    }
+})
