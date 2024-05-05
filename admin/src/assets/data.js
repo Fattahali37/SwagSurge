ca3b728ab6305b957fd072409c7899556ed6a2f5
Fattahali37 <fattahali37@gmail.com>
2024-05-05 18:08:07 +0500

    admin panel
    navbar + sidebar

---
+++ admin/src/assets/data.js
@@ -0,0 +1,37 @@
+import p1_product from "./woman1.png";
+import p2_product from "./woman2.png";
+import p3_product from "./woman3.png";
+import p4_product from "./woman4.png";
+
+let data_product = [
+    {
+        id: 1,
+        name: "Shirt1",
+        image: p1_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 2,
+        name: "Shirt2",
+        image: p2_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 3,
+        name: "Shirt3",
+        image: p3_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    },
+    {
+        id: 4,
+        name: "Shirt4",
+        image: p4_product,
+        new_price: 50.0,
+        old_price: 80.0,
+    }
+];
+
+export default data_product;
