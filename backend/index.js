506a17a92828ddc0efd56f888c468d2a0ecc1286
Fattahali37 <fattahali37@gmail.com>
2024-05-05 15:33:00 +0500

    api to remove
    incorrect image address + api to remove product has been added

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 //Database connectio with mongodb
 
-mongoose.connect("mongodb+srv://fattahali37:!babayaga37@cluster0.psbe9ke.mongodb.net/")
+mongoose.connect("mongodb+srv://fattahali37:!babayaga37@cluster0.psbe9ke.mongodb.net/e-commerce")
 
 //API creation:
 app.get("/",(req,res)=>{
@@ -79,10 +79,20 @@ const Product = mongoose.model("Product",{
 })
 
 app.post('/addproduct',async(req,res)=>{
+    let products = await Product.find({});
+    let id;
+    if(products.length>0)
+        {
+            let last_product_array = products.slice(-1);
+            let last_product = last_product_array[0];
+            id = last_product.id+1;
+        }else{
+            id=1;
+        }
     const product = new Product({
-        id:req.body.id,
+        id:id,
         name:req.body.name,
-        image:req.body.name,
+        image:req.body.image,
         category:req.body.category,
         new_price:req.body.new_price,
         old_price:req.body.old_price,
@@ -93,6 +103,16 @@ app.post('/addproduct',async(req,res)=>{
     res.json({
         success:true,
         name:req.body.name,
+    })
+})
+
+//creating api for deleting products
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
     })
 })
 
