607ecede1d303df6306e8c5df8448dbd3193a3cb
hubaidurrehman24 <hubaidurrehman03@gmail.com>
2024-05-07 11:55:03 +0500

    fetch data of popular in women,popular, and some dealing with carts

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -16,11 +16,24 @@ const ShopContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
-  // useEffect(()=>{
-  //   fetch('http://localhost:4000/allproducts')
-  //   .then((response)=>response.json())
-  //   .then((data)=>setAll_Product(data))
-  // },[])
+  useEffect(()=>{
+    fetch('http://localhost:4000/allproducts')
+    .then((response)=>response.json())
+    .then((data)=>setAll_Product(data))
+
+if(localStorage.getItem('auth-token')){
+  fetch('http://localhost:4000/getcart',{
+    method:'POST',
+    headers:{
+      Accept:'application/form-data',
+      'auth-token':`${localStorage.getItem('auth-token')}`,
+      "Content-Type":'application/json',
+    },
+    body:"",
+  }).then((response)=>response.json())
+  .then((data)=>setCartItems(data));
+}
+  },[])
 
   useEffect(() => {
     fetch('http://localhost:4000/allproducts')
@@ -37,9 +50,36 @@ const ShopContextProvider = (props) => {
 
   const addToCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+ if(localStorage.getItem('auth-token')){
+  fetch('http://localhost:4000/addtocart',{
+    method:'POST',
+    header:{
+      Accept:'application /form-data',
+      'auth-token':`${localStorage.getItem('auth-token')}`,
+      'COntent-Type':'application/json',
+    },
+    body:JSON.stringify({"itemId":itemId})
+  })
+  .then((response)=>response.json())
+  .then((data)=>console.log(data));
+ }
   };
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  if(localStorage.getItem('auth-token')){
+fetch('http://localhost:4000/removefromcart',{
+  method:'POST',
+  headers:{
+    Accept:'application/form-data',
+    'auth-token':`${localStorage.getItem('auth-toekn')}`,
+    'Content-Type':'application/json',
+  },
+  body:JSON.stringify({"itemId":itemId}),
+})
+.then((response)=>response.json())
+.then((data)=>console.log(data));
+  }
+  
   };
   const getTotalCartAmount = () => {
     let totalAmount = 0;
