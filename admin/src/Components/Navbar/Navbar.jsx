da93c3596cc6eb7a2e68773d1275eb215946f06a
Fattahali37 <fattahali37@gmail.com>
2024-05-13 20:02:08 +0500

    revert changes in admin

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,54 +1,15 @@
-import React, { useContext, useRef, useState } from 'react';
-import './Navbar.css';
-import cart_icon from '../Assets/cart_icon.png';
-import { Link } from 'react-router-dom';
-import { ShopContext } from '../../Context/ShopContext';
-import nav_dropdown from '../Assets/nav_dropdown.png';
+import React from 'react'
+import './Navbar.css'
+import navlogo from '../../assets/logo.png'
+import navProfile from '../../assets/prof.jpeg'
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("shop");
-  const { getTotalCartItems } = useContext(ShopContext);
-  const menuRef = useRef();
-
-  const dropdown_toggle = (e) => {
-    menuRef.current.classList.toggle('nav-menu-visible');
-    e.target.classList.toggle('open');
-  }
-
-  const handleLogout = () => {
-    localStorage.removeItem('auth-token');
-    window.location.replace('/');
-  }
-
   return (
     <div className='navbar'>
-      <div className='nav-logo'>
-        <p>SwagSurge</p>
-      </div>
-      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
-      <ul ref={menuRef} className="nav-menu">
-        <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link> {menu === "shop" ? <hr /> : <></>}</li>
-        <li onClick={() => { setMenu("men") }}><Link style={{ textDecoration: 'none' }} to='/men'>Men</Link>{menu === "men" ? <hr /> : <></>}</li>
-        <li onClick={() => { setMenu("women") }}><Link style={{ textDecoration: 'none' }} to='/women'>Women</Link>{menu === "women" ? <hr /> : <></>}</li>
-        <li onClick={() => { setMenu("kids") }}><Link style={{ textDecoration: 'none' }} to='./kids'>Kids</Link>{menu === "kids" ? <hr /> : <></>}</li>
-      </ul>
-      <div className="nav-login-cart">
-        {localStorage.getItem('auth-token') ? (
-          <div className="nav-account">
-            <button>Account</button>
-            <ul className="account-dropdown">
-              <li><Link to='/manage-account'>Manage Account</Link></li>
-              <li onClick={handleLogout}>Logout</li>
-            </ul>
-          </div>
-        ) : (
-          <Link to='/login'><button>Login</button></Link>
-        )}
-        <Link to='/Cart'><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">{getTotalCartItems()}</div>
-      </div>
+      <img src={navlogo} alt="" className="nav-logo" />
+      <img src={navProfile} alt="" className="nav-Profile" />
     </div>
   )
 }
 
-export default Navbar;
+export default Navbar
